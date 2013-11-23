d2dcef2ac95e69d2682fe0eecd480058e8b779b0
jmaguire <jmaguire@stanford.edu>
2013-11-23 15:51:46 -0500

    binary thres image

--- getTemplate.py
+++ getTemplate.py
@@ -13,7 +13,7 @@ class getTemplate:
         board = np.float32([ [w*3/8,h*1/8],[w*3/8,h*7/8],[w*5/8,h*7/8],[w*5/8,h*1/8],\
                 [w*1/8,h*3/8],[w*7/8,h*3/8],[w*1/8,h*5/8],[w*7/8,h*5/8]]).reshape(-1,1,2)    
         start = time.time()
-        while(rval and time.time() - start < 5):
+        while(rval and time.time() - start < 8):
             for i in [0,2,4,6]:
                 #print newBoard[i][0],newBoard[i+1][0]
                 pt1 = tuple(board[i][0])
@@ -22,11 +22,19 @@ class getTemplate:
             cv2.imshow('preview', frame)
             rval, frame = vc.read()
             key = cv2.waitKey(5)
-        avg = np.float32(frame)
         rval, frame = vc.read()
         key = cv2.waitKey(5)
         winsound.Beep(getTemplate.FREQ,getTemplate.DURATION)
+        
       
+        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        imgSplit = cv2.split(frame)
+        gray = cv2.GaussianBlur(gray,(5,5),0)
+        thres = cv2.adaptiveThreshold(gray,255,1,1,11,2) 
+        
+        cv2.imshow('preview', thres)
+        key = cv2.waitKey(5)
+        time.sleep(5.5)  
         return frame
         
         
