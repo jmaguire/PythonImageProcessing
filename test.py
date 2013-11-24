74f1609e9fc6973111d854262cb710db0a5a9f6a
jmaguire <jmaguire@stanford.com>
2013-11-24 12:03:54 -0800

    better organized

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import numpy as np
 import getTemplate as gt
 
 ## Image/Dectector Class
-class Frame:
+class CapturedImage:
     def __init__(self,image,descriptors,keypoints):
         self.image = image
         self.descriptors = descriptors
@@ -24,8 +24,9 @@ o = gt.getTemplate()
 frame = o.getImage(4)     
 image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 keypoints,descriptors = detector.detectAndCompute(image,None)   
-target = Frame(image, descriptorsTarget,keypointsTarget)
-print target.getImage() == image,target.getDescriptors() == descriptorsTarget, target.getKeypoints() == keypointsTarget
+target = CapturedImage(image, descriptors,keypoints)
+
+
 ##Initialize webcam
 cv2.namedWindow("preview")
 vc = cv2.VideoCapture(0)
@@ -38,15 +39,27 @@ if vc.isOpened(): # try to get the first frame should always work!
 else:
     rval = False 
     
-    
-def getMatches(descriptors, descriptorsTarget):
+## Get Matches. Return empty array if no descriptors are found
+def getMatches(captured, target):
+    ##initialize good matches
+    goodMatches = []
+
+    ## Create Flann Tree
     FLANN_INDEX_KDTREE = 0
     index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-    search_params = dict(checks = 100)
+    search_params = dict(checks = 75)
     flann = cv2.FlannBasedMatcher(index_params, search_params)
-    matches = flann.knnMatch(descriptorsTarget,descriptors,k=2)
+    
+    ## Check edge cases
+    if (captured.getDescriptors() is None or target.getDescriptors() is None):
+        return goodMatches
+    
+    if (len(captured.getDescriptors()) == 0 or len(target.getDescriptors()) == 0):
+        return goodMatches
+    
+    matches = flann.knnMatch(target.getDescriptors(),captured.getDescriptors(),k=2)
     # store all the good matches as per Lowe's ratio test.
-    goodMatches = []
+    
     for m,n in matches:
         if m.distance < 0.7*n.distance:
             goodMatches.append(m)
@@ -55,36 +68,55 @@ def getMatches(descriptors, descriptorsTarget):
 
 
 ##Build stuff. TEMPORARY. Should extract board from target
-h,w = target.shape
+h,w = target.getImage().shape
+print h,w
+
 board = np.float32([ [w*3/8,h*1/8],[w*3/8,h*7/8],[w*5/8,h*7/8],[w*5/8,h*1/8],\
     [w*1/8,h*3/8],[w*7/8,h*3/8],[w*1/8,h*5/8],[w*7/8,h*5/8]]).reshape(-1,1,2)    
 
-'''
+## Outline of original image
+pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+
+## Homography
+H = None
+
 while True:
+    ## Get Matches
     im = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     keypoints,descriptors = detector.detectAndCompute(im,None)
-    if descriptors is None: continue
-    if len(descriptors) > 3:
-        matches = getMatches(descriptors, descriptorsTarget)
-        if len(matches) > 10:
-            src_pts = np.float32([ keypointsTarget[m.queryIdx].pt for m in matches ]).reshape(-1,1,2)
-            dst_pts = np.float32([ keypoints[m.trainIdx].pt for m in matches ]).reshape(-1,1,2)
-            M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
-            matchesMask = mask.ravel().tolist()
-            
-            pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-            dst = cv2.perspectiveTransform(pts,M)
-            newBoard = np.int32(cv2.perspectiveTransform(board,M));
-            for i in [0,2,4,6]:
-                #print newBoard[i][0],newBoard[i+1][0]
-                pt1 = tuple(newBoard[i][0])
-                pt2 = tuple(newBoard[i+1][0])
-                cv2.line(frame,pt1,pt2,[255,0,0],thickness = 2)
-                
-            for m in matches:
-                center = (int(keypoints[m.trainIdx].pt[0]),int(keypoints[m.trainIdx].pt[1]))
-                cv2.circle(frame,center,10,(255,0,0))
-            img2 = cv2.polylines(frame,[np.int32(dst)],True,255,3)
+    captured = CapturedImage(im, descriptors,keypoints)
+    
+    matches = getMatches(captured, target)
+    
+    ## Only update Homography and board if we can see it
+    print leen(matches)
+    if len(matches) > 2:
+        ## Get points, find Homography
+        src_pts = np.float32([ target.getKeypoints()[m.queryIdx].pt for m in matches ]).reshape(-1,1,2)
+        dst_pts = np.float32([ captured.getKeypoints()[m.trainIdx].pt for m in matches ]).reshape(-1,1,2)
+        H, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,6.0)
+        matchesMask = mask.ravel().tolist()
+        
+        ## Check for X's
+        
+        ## update board
+
+        
+        
+    ## draw board. 
+    if H is not None: 
+        dst = cv2.perspectiveTransform(pts,H)
+        ## Draw tictactoe board
+        newBoard = np.int32(cv2.perspectiveTransform(board,H));
+        for i in [0,2,4,6]:
+            ## end points of lines
+            pt1 = tuple(newBoard[i][0])
+            pt2 = tuple(newBoard[i+1][0])
+            cv2.line(frame,pt1,pt2,[255,0,0],thickness = 2)
+        # for m in matches:
+            # center = (int(keypoints[m.trainIdx].pt[0]),int(keypoints[m.trainIdx].pt[1]))
+            # cv2.circle(frame,center,10,(255,0,0))
+        img2 = cv2.polylines(frame,[np.int32(dst)],True,255,3)
         
     cv2.imshow('preview', frame)
     rval, frame = vc.read()
@@ -93,5 +125,5 @@ while True:
     
     if key == 27: # exit on ESC
         break
-'''
+
 cv2.destroyWindow("preview")
